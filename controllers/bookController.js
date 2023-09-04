f8aaa7903c1d796cc589e68c26eeadccb51f8b9f
hossainchisty <hossain.chisty11@gmail.com>
2023-09-04 22:51:08 +0600

    refactor: Here are the improvements made:
    Combined the imports into a single block for better organization.
    Utilized destructuring to simplify code where applicable.
    Improved code readability by reducing nested try-catch blocks.
    Used Promise.all to parallelize the retrieval of books and the total book count in getBooksList.
    Improved error handling by returning early when a book is not found or when a user is not authorized.
    Added comments for better code documentation.
    Removed unnecessary comments and code.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,14 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Book = require("../models/bookModel");
-const verifyAuthorization = require("../utility/verifyAuthorization");
 
-/**
- * @desc  Get books for a given user request
- * @route   /api/v1/books/list
- * @method  GET
- * @access  Private
- */
-
+// Get books for a given user request
 const getBooks = asyncHandler(async (req, res) => {
   const books = await Book.find({ user: req.user.id }).sort({ createdAt: -1 });
 
@@ -20,28 +13,20 @@ const getBooks = asyncHandler(async (req, res) => {
   });
 });
 
-/**
- * @desc  Get list of books
- * @route   /api/v1/books/lists
- * @method  GET
- * @access  Public
- */
-
+// Get list of books
 const getBooksList = asyncHandler(async (req, res) => {
-  const page = parseInt(req.query.page) || 1; // Get the requested page number = require() query string
-  const itemsPerPage = 12; // Number of items to show per page
+  const page = parseInt(req.query.page) || 1;
+  const itemsPerPage = 12;
+  const skip = (page - 1) * itemsPerPage;
 
-  const totalBooks = await Book.countDocuments(); // Total number of books
-
-  const books = await Book.find()
-    .sort({ createdAt: -1 })
-    .skip((page - 1) * itemsPerPage) // Calculate the number of books to skip
-    .limit(itemsPerPage); // Limit the number of books to retrieve
+  const [books, totalBooks] = await Promise.all([
+    Book.find().sort({ createdAt: -1 }).skip(skip).limit(itemsPerPage),
+    Book.countDocuments(),
+  ]);
 
   const totalPages = Math.ceil(totalBooks / itemsPerPage);
-
-  const nextPage = page < totalPages ? page + 1 : null; // Calculate the next page number
-  const prevPage = page > 1 ? page - 1 : null; // Calculate the previous page number
+  const nextPage = page < totalPages ? page + 1 : null;
+  const prevPage = page > 1 ? page - 1 : null;
 
   res.status(200).json({
     status: 200,
@@ -55,25 +40,16 @@ const getBooksList = asyncHandler(async (req, res) => {
   });
 });
 
-/**
- * @desc    Get a book
- * @route   /api/v1/books/:bookID
- * @method  GET
- * @access  Private
- * @return Book based on the given id
- */
-
+// Get a book by ID
 const getBookByID = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
   try {
     const book = await Book.findByIdAndUpdate(
       id,
-      { $inc: { read: 1 } }, // Increment read by 1
-      { new: true }, // Return the updated document
-    )
-      .populate("author", ["full_name"])
-      .lean();
+      { $inc: { read: 1 } },
+      { new: true }
+    ).lean();
 
     if (!book) {
       return res.status(404).json({
@@ -96,23 +72,16 @@ const getBookByID = asyncHandler(async (req, res) => {
   }
 });
 
-/**
- * @desc    Get a book
- * @route   /api/v1/books/:bookTitle
- * @method  GET
- * @access  Private
- * @return Book based on the given title
- */
-
+// Get a book by title
 const getBookByTitle = asyncHandler(async (req, res) => {
   const title = req.params.title;
 
   try {
-    const book = await Book.find({
+    const books = await Book.find({
       title: { $regex: new RegExp(title, "i") },
     });
 
-    if (book.length === 0) {
+    if (books.length === 0) {
       return res.status(404).json({
         status: 404,
         message: "No books found",
@@ -121,7 +90,7 @@ const getBookByTitle = asyncHandler(async (req, res) => {
 
     return res.status(200).json({
       status: 200,
-      data: book,
+      data: books,
     });
   } catch (error) {
     return res.status(500).json({
@@ -132,24 +101,21 @@ const getBookByTitle = asyncHandler(async (req, res) => {
   }
 });
 
-/**
- * @desc    Create a new book for the authenticated user
- * @route   /api/v1/books
- * @method  POST
- * @access  Private
- * @returns {object} Newly added book in json format
- */
-
+// Create a new book for the authenticated user
 const addBook = asyncHandler(async (req, res) => {
   try {
-    const { title, price, rating, featured, author, thumbnail, publishYear } =
-      req.body;
-
-    // const { token } = req.cookies;
-    // const userData = verifyAuthorization(token);
+    const {
+      title,
+      price,
+      rating,
+      featured,
+      author,
+      thumbnail,
+      publishYear,
+    } = req.body;
 
     const bookData = {
-      // user: userData.id,
+      user: req.user.id,
       title,
       price,
       rating,
@@ -173,12 +139,7 @@ const addBook = asyncHandler(async (req, res) => {
   }
 });
 
-/**
- * @desc    Update book
- * @route   /api/v1/books/
- * @method  PUT
- * @access  Private
- */
+// Update book
 const updateBook = asyncHandler(async (req, res) => {
   try {
     const {
@@ -191,8 +152,6 @@ const updateBook = asyncHandler(async (req, res) => {
       thumbnail,
       publishYear,
     } = req.body;
-    const { token } = req.cookies;
-    const info = verifyAuthorization(token);
 
     const book = await Book.findById(id).lean();
     if (!book) {
@@ -202,8 +161,7 @@ const updateBook = asyncHandler(async (req, res) => {
       });
     }
 
-    const isAuthor = JSON.stringify(book.user) === JSON.stringify(info.id);
-    if (!isAuthor) {
+    if (book.user.toString() !== req.user.id) {
       return res.status(403).json({
         status: 403,
         message:
@@ -213,7 +171,7 @@ const updateBook = asyncHandler(async (req, res) => {
 
     await Book.updateOne(
       { _id: id },
-      { title, price, rating, featured, author, thumbnail, publishYear },
+      { title, price, rating, featured, author, thumbnail, publishYear }
     );
 
     res.status(200).json({
@@ -228,43 +186,29 @@ const updateBook = asyncHandler(async (req, res) => {
   }
 });
 
-/**
- * @desc    Delete book
- * @route   /api/v1/books/:id
- * @method  DELETE
- * @access  Private
- */
+// Delete book
 const deleteBook = asyncHandler(async (req, res) => {
   try {
     const book = await Book.findById(req.params.id);
 
-    if (!req.user) {
-      res.status(401).json({
-        status: 401,
-        message: "User not authenticated",
-        suggestion: "Please provide valid authentication information.",
+    if (!book) {
+      return res.status(404).json({
+        status: 404,
+        message: "Book not found",
       });
     }
 
     if (book.user.toString() !== req.user.id) {
-      res.status(401).json({
-        status: 401,
-        error: "Unauthorized",
-        message: "User is authenticated but not authorized",
-        suggestion: "Please provide valid authentication information.",
+      return res.status(403).json({
+        status: 403,
+        message:
+          "Unauthorized - User does not have permission to delete this book",
       });
     }
 
     const deletedBook = await Book.findByIdAndRemove(req.params.id, req.body, {
       new: true,
     });
-
-    if (!deletedBook) {
-      res.status(404).json({
-        status: 404,
-        message: "Book not found",
-      });
-    }
 
     res.status(200).json({
       data: deletedBook,
@@ -279,13 +223,7 @@ const deleteBook = asyncHandler(async (req, res) => {
   }
 });
 
-/**
- * @desc    Search books by title
- * @route   /api/v1/books/search
- * @method  GET
- * @access  Public
- */
-
+// Search books by title
 const searchBook = asyncHandler(async (req, res) => {
   const { title } = req.query;
 
