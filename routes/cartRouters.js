28108abb6f7cfaaa4098e41fbead005e3bb98f53
hossainchisty <hossainchisty@users.noreply.github.com>
2023-09-25 20:23:39 +0000

    style: format files

--- routes/cartRouters.js
+++ routes/cartRouters.js
@@ -6,7 +6,7 @@ const {
   addToCart,
   getUserCart,
   removeItemFromCart,
-  removeAllItemsFromCart
+  removeAllItemsFromCart,
 } = require("../controllers/cartController");
 
 const { authMiddleware } = require("../middleware/authMiddleware");
