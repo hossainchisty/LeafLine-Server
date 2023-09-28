1c87f671f14bf29387edc13c705eb0c8754f4844
hossainchisty <hossain.chisty11@gmail.com>
2023-09-28 19:50:06 +0600

    refactor: change function descriptn

--- services/errorService.js
+++ services/errorService.js
@@ -3,18 +3,18 @@
  *
  * @param {Object} res - Express response object.
  * @param {number} statusCode - HTTP status code for the error response.
+ * @param {boolean} success - Indicates whether the operation was successful.
  * @param {string} message - A message describing the error.
  * @returns {Object} - The error response object sent to the client.
  */
-function sendError(res, statusCode, message) {
-    return res.status(statusCode).json({
-      statusCode,
-      success,
-      message,
-    });
-  }
-  
-  module.exports = {
-    sendError,
-  };
-  
+function sendError(res, statusCode, success, message) {
+  return res.status(statusCode).json({
+    statusCode,
+    success,
+    message,
+  });
+}
+
+module.exports = {
+  sendError,
+};
