a1e4fe6df92016cfc28d8cb53125eb28a140ffa7
hossainchisty <hossain.chisty11@gmail.com>
2023-09-28 19:22:03 +0600

    fix: Added data object to res API result

--- services/responseService.js
+++ services/responseService.js
@@ -1,13 +1,20 @@
 /**
  * Send a standardized JSON response to the client.
  *
- * @param {Object} res - Express response object.
- * @param {number} statusCode - HTTP status code for the response.
+ * @param {Object} res - The Express response object.
+ * @param {number} statusCode - The HTTP status code for the response.
  * @param {boolean} success - Indicates whether the operation was successful.
  * @param {string} message - A message to include in the response.
  * @param {Object} [data] - Optional data to include in the response.
  * @returns {Object} - The response object sent to the client.
+ *
+ * This function generates a standardized JSON response to send to the client.
+ * It allows you to specify the HTTP status code, success status, an optional message,
+ * and optional data to include in the response.
+ * If a message or data is not provided, it will be set to null in the response.
+ * The response object conforms to the specified structure for consistency.
  */
+
 function sendResponse(res, statusCode, success, message, data) {
   const response = {
     statusCode,
@@ -15,7 +22,7 @@ function sendResponse(res, statusCode, success, message, data) {
     message,
   };
 
-  if (data) {
+  if (data !== undefined && data !== null) {
     response.data = data;
   }
 
@@ -25,5 +32,4 @@ function sendResponse(res, statusCode, success, message, data) {
 module.exports = {
   sendResponse,
 };
-
   
