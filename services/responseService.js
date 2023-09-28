2d67657643557d0ac0e20bf84e4fd6ef83c81588
hossainchisty <hossain.chisty11@gmail.com>
2023-09-28 17:38:24 +0600

    refactor: added optioinal data param

--- services/responseService.js
+++ services/responseService.js
@@ -5,17 +5,25 @@
  * @param {number} statusCode - HTTP status code for the response.
  * @param {boolean} success - Indicates whether the operation was successful.
  * @param {string} message - A message to include in the response.
+ * @param {Object} [data] - Optional data to include in the response.
  * @returns {Object} - The response object sent to the client.
  */
-function sendResponse(res, statusCode, success, message) {
-    return res.status(statusCode).json({
-      statusCode,
-      success,
-      message,
-    });
-  }
-  
-  module.exports = {
-    sendResponse,
+function sendResponse(res, statusCode, success, message, data) {
+  const response = {
+    statusCode,
+    success,
+    message,
   };
+
+  if (data) {
+    response.data = data;
+  }
+
+  return res.status(statusCode).json(response);
+}
+
+module.exports = {
+  sendResponse,
+};
+
   
