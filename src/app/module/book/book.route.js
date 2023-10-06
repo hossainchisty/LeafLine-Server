b7706b0aa5a4a589a96f77689f6b409a11a5ca75
hossainchisty <hossain.chisty11@gmail.com>
2023-10-06 18:28:58 +0600

    refactor: Added auth middleware

--- src/app/module/book/book.route.js
+++ src/app/module/book/book.route.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const bookController = require('./book.controller');
+const { authMiddleware } = require('../../middleware/authMiddleware');
 
 // API Endpoints for Managing Books
-router.route('/').post(bookController.addBook).put(bookController.updateBook);
+router.route('/').post(bookController.addBook, authMiddleware).put(bookController.updateBook);
 router.get('/list', bookController.getBooksList);
 router.get('/search', bookController.searchBook);
 router.get('/:id', bookController.getBookByID);
