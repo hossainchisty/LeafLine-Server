61db39c0e17c9fa08e73e42c37001a50d1ca1f11
hossainchisty <hossain.chisty11@gmail.com>
2023-10-18 20:50:56 +0600

    feat: Get monthly sales from the orders by aggregating the data based on the order creation dates
    Issue resolve: #27

--- src/app/module/admin/admin.controller.js
+++ src/app/module/admin/admin.controller.js
@@ -1,10 +1,11 @@
-const asyncHandler = require("express-async-handler");
-const Book = require("../book/book.model");
-const User = require("../user/user.model");
-const Order = require("../order/order.model");
+const asyncHandler = require('express-async-handler');
+const Book = require('../book/book.model');
+const User = require('../user/user.model');
+const Order = require('../order/order.model');
+const { sendResponse } = require('../../../services/responseService');
 
 // Get analytics for admin panel
-const dashboard = asyncHandler(async (req, res) => {
+exports.dashboard = asyncHandler(async (req, res) => {
   const total_books = await Book.countDocuments();
   const featured_books = await Book.countDocuments({ featured: true });
   const total_users = await User.countDocuments();
@@ -13,17 +14,47 @@ const dashboard = asyncHandler(async (req, res) => {
     status: 'Delivered',
   });
 
-  res.status(200).json({
-    success: true,
-    statusCode: 200,
-    message: "Analytics fetched successfully",
-    data: {
-      total_orders,
-      total_books,
-      featured_books,
-      total_users,
-    },
-  });
+  const data = {
+    total_orders,
+    total_books,
+    featured_books,
+    total_users,
+  };
+  return sendResponse(res, 200, true, 'Analytics fetched successfully', data);
 });
 
-module.exports = dashboard;
+// Get monthly sales chart data
+exports.monthlySales = asyncHandler(async (req, res) => {
+  try {
+    const monthlySales = await Order.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: new Date('2023-01-01'), // Start date
+            $lte: new Date('2023-12-31'), // End date
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: '$createdAt' },
+            month: { $month: '$createdAt' },
+          },
+          totalSales: { $sum: '$totalPrice' },
+        },
+      },
+      {
+        $sort: {
+          '_id.year': 1,
+          '_id.month': 1,
+        },
+      },
+    ]);
+
+    return sendResponse(res, 200, true, 'Monthly sales fetched successfully', monthlySales);
+  } catch (error) {
+    console.error('Error fetching last sales:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
